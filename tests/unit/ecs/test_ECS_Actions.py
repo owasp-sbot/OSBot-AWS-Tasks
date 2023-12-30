de6e861985b2147ef233da9fa4923e90aa0bf867
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-30 16:52:30 +0000

    started adding ECS Cluster tasks

--- tests/unit/ecs/test_ECS_Actions.py
+++ tests/unit/ecs/test_ECS_Actions.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from osbot_utils.utils.Dev import pprint
+
 from osbot_aws.apis.Cloud_Watch_Logs import Cloud_Watch_Logs
 from osbot_aws.apis.ECS import ECS
 from osbot_aws.apis.STS import STS
@@ -13,14 +15,25 @@ class test_ECS_Actions(TestCase):
         self.ecs_actions = ECS_Actions()
 
     def test__init__(self):
-        assert type(self.ecs_actions.ecs) is ECS
+        assert type(self.ecs_actions.ecs    ) is ECS
+        assert type(self.ecs_actions.logs   ) is Cloud_Watch_Logs
+        assert self.ecs_actions.account_id
+        assert self.ecs_actions.region
+        assert self.ecs_actions.account_id    == self.ecs_actions.logs.account_id
+        assert self.ecs_actions.region        == self.ecs_actions.logs.region_name
 
-    def test_ecs_setup(self):
-        STS().check_current_session_credentials()
-        self.ecs          = ECS()
-        self.logs         = Cloud_Watch_Logs()
-        self.account_id   = self.ecs.account_id
-        self.region       = self.ecs.region
-        self.cluster_name = 'test_ecs_cluster_2'
-        self.task_family  = 'test_task_create'
-        self.cluster_arn  = self.ecs.cluster_arn(self.cluster_name)
+    def test_create_cluster(self):
+        cluster = self.ecs_actions.create_cluster()
+        assert cluster.get('clusterArn'       ) == self.ecs_actions.cluster_arn
+        assert cluster.get('clusterName'      ) == self.ecs_actions.cluster_name
+        assert cluster.get('runningTasksCount')==0
+        assert self.ecs_actions.cluster_arn in self.ecs_actions.ecs.clusters_arns()
+        assert self.ecs_actions.ecs.cluster_exists(cluster_arn=self.ecs_actions.cluster_arn) is True
+
+    def test_delete_cluster(self):
+        result = self.ecs_actions.delete_cluster()
+        pprint(result)
+
+    def test_info_cluster(self):
+        result = self.ecs_actions.info_cluster()
+        assert result == []
